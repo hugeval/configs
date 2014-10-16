cc8889492a68f60505c270b9c212a54309488118
hugeval <hugeval@hugeval>
2014-10-16 18:10:46 +0300

    Ubuntu packages

---
+++ ubuntu.conf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get install aptitude
+aptitude install htop mc php5 php5-mcrypt php5-curl php5-apcu php5-gd php5-intl php5-json php5-mysql php5-mysqli php5-oauth php5-xdebug phpunit phpunit-selenium phpunit-story mysql-server-5.5 node node-requirejs git
+aptitude install compizconfig-settings-manager keepassx nautilus-dropbox gimp vlc
